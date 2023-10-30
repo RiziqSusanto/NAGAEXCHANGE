cfe13a6c9e922f0880b5cc69b4ea21bce770cf8e
RiziqSusanto <riziq.susanto@gmail.com>
2023-10-30 18:56:51 +0700

    Feat: remove unused

--- src/screens/Detail.tsx
+++ src/screens/Detail.tsx
@@ -1,17 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Alert,
-  ImageBackground,
-  TextInput,
-  FlatList,
-  Image,
-} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
 import {DetailScreenNavigationProp, DetailScreenRouteProp} from '../navigation';
-import request from 'superagent';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 const DetailScreen: React.FC<{
   navigation: DetailScreenNavigationProp;
